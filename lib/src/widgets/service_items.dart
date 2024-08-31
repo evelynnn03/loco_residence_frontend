879d95fd27fbe743d1a7710be23b0396a9053e4e
evelynnn03 <evelynthj@gmail.com>
2024-08-31 13:58:11 +0800

    updated call function

--- lib/src/widgets/service_items.dart
+++ lib/src/widgets/service_items.dart
@@ -17,116 +17,101 @@ class ServiceItems extends StatefulWidget {
 }
 
 class _ServiceItemsState extends State<ServiceItems> {
+  bool isPressed = false;
+
   @override
   Widget build(BuildContext context) {
     final mode = Provider.of<ThemeProvider>(context);
     // Color backgroundColor = Theme.of(context).cardColor;
     Color boxShadowColor = Color.fromRGBO(129, 101, 234, 1);
     final screenHeight = MediaQuery.of(context).size.height;
-    bool isPressed = false;
-
     double containerHeight = screenHeight * 0.32;
     double imageHeight = screenHeight * 0.2;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: Container(
-        height: containerHeight,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          boxShadow: mode.isDark
-              ? [
-                  BoxShadow(
-                    color: boxShadowColor,
-                    offset: const Offset(
-                      0.0,
-                      0.0,
-                    ),
-                    blurRadius: 5.0,
-                    spreadRadius: 1.0,
-                  ), //BoxShadow
-                  //BoxShadow
-                ]
-              : null,
-          color: GlobalVariables
-              .primaryColor, // Set the background color to purple
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: CachedNetworkImage(
-                imageUrl: widget.imageUrl,
-                width: double.infinity,
-                height: imageHeight,
-                fit: BoxFit.cover,
-                errorWidget: (context, url, error) => Icon(
-                  Icons.error,
-                  size: GlobalVariables.responsiveIconSize(context, 60),
-                  color: GlobalVariables.greyishPurple,
+      child: GestureDetector(
+        onTap: () async {
+          final uri = Uri.parse('tel:${widget.phoneNum}');
+          if (await canLaunchUrl(uri)) {
+            await launchUrl(uri);
+          } else {
+            print('Unable to make a call');
+          }
+        },
+        child: Container(
+          height: containerHeight,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            boxShadow: mode.isDark
+                ? [
+                    BoxShadow(
+                      color: boxShadowColor,
+                      offset: const Offset(
+                        0.0,
+                        0.0,
+                      ),
+                      blurRadius: 5.0,
+                      spreadRadius: 1.0,
+                    ), //BoxShadow
+                    //BoxShadow
+                  ]
+                : null,
+            color: GlobalVariables
+                .primaryColor, // Set the background color to purple
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: CachedNetworkImage(
+                  imageUrl: widget.imageUrl,
+                  width: double.infinity,
+                  height: imageHeight,
+                  fit: BoxFit.cover,
+                  errorWidget: (context, url, error) => Icon(
+                    Icons.error,
+                    size: GlobalVariables.responsiveIconSize(context, 60),
+                    color: GlobalVariables.greyishPurple,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(widget.title,
-                      style: GlobalVariables.importantTitleStyle(context)),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.phone_in_talk,
-                        color: GlobalVariables.white,
-                        size: GlobalVariables.responsiveIconSize(context, 23),
-                      ),
-                      GestureDetector(
-                        onTapDown: (_) {
-                          setState(() {
-                            isPressed = true;
-                            print('Tapped down, isPressed: $isPressed');
-                          });
-                        },
-                        onTapUp: (_) {
-                          setState(() {
-                            isPressed = false;
-                            print('Tapped up, isPressed: $isPressed');
-                          });
-                        },
-                        onTapCancel: () {
-                          setState(() {
-                            isPressed = false;
-                            print('Tap canceled, isPressed: $isPressed');
-                          });
-                        },
-                        onTap: () async {
-                          final uri = Uri.parse('tel:${widget.phoneNum}');
-                          if (await canLaunchUrl(uri)) {
-                            await launchUrl(uri);
-                          } else {
-                            print('Unable to make a call');
-                          }
-                        },
-                        child: Text(
-                          '  ${widget.phoneNum}',
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(widget.title,
+                        style: GlobalVariables.importantTitleStyle(context)),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.phone_in_talk,
+                          color: GlobalVariables.white,
+                          size: GlobalVariables.responsiveIconSize(context, 23),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          '${widget.phoneNum}',
                           style: TextStyle(
                             fontSize: GlobalVariables.responsiveFontSize(
                                 context, 17.0),
-                            color: isPressed ? Colors.green : Colors.white,
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
